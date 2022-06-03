36da660f90c8ecb2f4feb4a8baeb0de39b102e39
Pushkar1853 <pambasthabiz1853@gmail.com>
2022-06-03 19:31:37 +0530

    Js and carousel changes

--- js/nav.js
+++ js/nav.js
@@ -13,13 +13,6 @@ const createNav = () => {
                 <a href="#"><img src="img/img/cart3.svg" alt="" /></a>
             </div>
         </div>
-        <ul class="links-container">
-            <li class="link-item"><a href="#" class="link">home</a></li>
-            <li class="link-item"><a href="#" class="link">women</a></li>
-            <li class="link-item"><a href="#" class="link">men</a></li>
-            <li class="link-item"><a href="#" class="link">kids</a></li>
-            <li class="link-item"><a href="#" class="link">accessories</a></li>
-        </ul>
     `;
 };
 
