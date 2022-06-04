843d2765d473fa43b0b921cf46fbd01f62dc21d4
Pushkar1853 <pambasthabiz1853@gmail.com>
2022-06-04 17:32:25 +0530

    Searching Bar in new Project

--- js/home.js
+++ js/home.js
@@ -1,19 +1,3 @@
-// const productContainers = [...document.querySelectorAll(".product-container")];
-// const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
-// const preBtn = [...document.querySelectorAll(".pre-btn")];
-
-// productContainers.forEach((item, i) => {
-//   let containerDimenstions = item.getBoundingClientRect();
-//   let containerWidth = containerDimenstions.width;
-
-//   nxtBtn[i].addEventListener("click", () => {
-//     item.scrollLeft += containerWidth;
-//   });
-
-//   preBtn[i].addEventListener("click", () => {
-//     item.scrollLeft -= containerWidth;
-//   });
-// });
 
 var multipleCardCarousel = document.querySelector("#carouselExampleControls");
 if (window.matchMedia("(min-width: 768px)").matches) {
