6534400d0216b08e63a425db21c8c8aab1d92003
Pushkar1853 <pambasthabiz1853@gmail.com>
2022-06-04 19:24:51 +0530

    About ME

--- js/home.js
+++ js/home.js
@@ -46,3 +46,43 @@ $(".carousel .carousel-item").each(function () {
     next.children(":first-child").clone().appendTo($(this));
   }
 });
+
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  users.forEach((user) => {
+    const isVisible =
+      user.title.toLowerCase().includes(value) ||
+      user.brand.toLowerCase().includes(value);
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
+
+fetch("./product_dummy_data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    users = data.map((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      const footer = card.querySelector("[data-footer]");
+      const img = card.querySelector("[img-thumbnail]");
+      header.textContent = user.title;
+      body.textContent = user.brand;
+      footer.textContent = user.price;
+      img.src = user.thumbnail;
+      userCardContainer.append(card);
+      return {
+        title: user.title,
+        brand: user.brand,
+        description: user.description,
+        thumbnail: user.thumbnail,
+        element: card,
+      };
+    });
+  });
--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,43 @@
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase();
+  users.forEach((user) => {
+    const isVisible =
+      user.title.toLowerCase().includes(value) ||
+      user.brand.toLowerCase().includes(value);
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
+
+fetch("./product_dummy_data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    users = data.map((user) => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      const footer = card.querySelector("[data-footer]");
+      const img = card.querySelector("[img-thumbnail]");
+      header.textContent = user.title;
+      body.textContent = user.brand;
+      footer.textContent = user.price;
+      img.src = user.thumbnail;
+      userCardContainer.append(card);
+      return {
+        title: user.title,
+        brand: user.brand,
+        description: user.description,
+        thumbnail: user.thumbnail,
+        element: card,
+      };
+    });
+  });
+
 const createNav = () => {
   let nav = document.querySelector(".navbar");
 
@@ -5,10 +45,6 @@ const createNav = () => {
         <div class="nav">
         <img src="img/attachment_76170693.png" class="brand-logo" alt="" />
             <div class="nav-items">
-                <div class="search">
-                    <input type="text" class="search-box" placeholder="Search Product">
-                    <button class="search-btn">search</button>
-                </div>
                 <a href="#"><img src="img/img/person.svg" alt="" /></a>
                 <a href="#"><img src="img/img/cart3.svg" alt="" /></a>
             </div>
